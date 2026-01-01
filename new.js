3591581d0bb21f63fcd18af927e7848b8e14f9f0
jitbro <engrjeet98@gmail.com>
2026-01-01 22:30:00 +0600

    new

---
+++ new.js
@@ -0,0 +1,5 @@
+// Make the button interactive
+document.querySelector('button').addEventListener('click', function() {
+    alert('Button clicked!');
+    document.body.style.backgroundColor = 'lightblue';
+});
